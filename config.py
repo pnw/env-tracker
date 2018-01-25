ebdce261d4f5177d4d85fe0f330e7c011a979ef5
Patrick Walsh <patrick.nilsen.walsh@gmail.com>
2018-01-25 14:20:13 +0200

    make config safer by accomodating empty environment values

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
 
-ET_HOME = Path(os.environ.get('ET_HOME', str(Path.home() / '.et')))
+ET_HOME = Path(os.environ.get('ET_HOME', str(Path.home() / '.et'))) or '.et'
 
-PARENT_SYMLINK_NAME = os.environ.get('ET_PARENT_SYMLINK_NAME', '.source')
+PARENT_SYMLINK_NAME = os.environ.get('ET_PARENT_SYMLINK_NAME', '.source') or '.source'
